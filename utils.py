daea0b9d7aa795e2a3e339cda2106cc1f65a152b
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-26 19:28:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def load_plugins(plugin_name):
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltronX.modules." + plugin_name] = load
+    sys.modules["DARK.modules." + plugin_name] = load
     print("DARK has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
